ee49ee305b2e6f6f4600d746393ce1c5c23951f2
Abagail Hudson <abagailanne@gmail.com>
2019-06-06 15:48:34 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -23,7 +23,7 @@ class Scraper
 
   def self.scrape_profile_page(profile_url)
     doc = Nokogiri::HTML(open(profile_url))
-     student = Hash.new
+     student = {}
 
 
      social_icons = doc.css("div.social-icon-container a").collect {|x| x.attribute("href").value}
