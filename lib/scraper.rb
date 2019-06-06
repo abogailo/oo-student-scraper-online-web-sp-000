5fef37c5a66a8e5b792f81bba429cc7512470f52
Abagail Hudson <abagailanne@gmail.com>
2019-06-06 14:52:46 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,7 +7,7 @@ class Scraper
   def self.scrape_index_page(index_url)
      doc = Nokogiri::HTML(open(index_url))
 
-    students = []
+    student_hash = []
     doc.css("div.roster-cards-container").each do |card|
       card.css(".student-card a").each do |student|
         name = student.css(".student-name").text
@@ -16,7 +16,7 @@ class Scraper
         students << {name: name, location: location, profile_url: profile_url}
       end
     end
-    students
+    student_hash
   end
 
   def self.scrape_profile_page(profile_url)
