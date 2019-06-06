9f40a4b4b4d522f36594bd305768c776567df450
Abagail Hudson <abagailanne@gmail.com>
2019-06-06 14:54:53 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -12,8 +12,8 @@ class Scraper
     doc.css("div.roster-cards-container").each do |card|
       card.css(".student-card a").each do |student|
         student_hash <<{
-        name: = student.css(".student-name").text
-        location: = student.css(".student-location").text
+        name: = student.css(".student-name").text,
+        location: = student.css(".student-location").text,
         profile_url: = "#{student.attr("href")}"
       }
       end
